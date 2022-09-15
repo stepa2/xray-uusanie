1cf295b2d4efdcb2f780ec6301306a7c453d79d2
stpM64 <sssstepa452@yandex.ru>
2022-09-16 00:31:19 +0300

    Added Shader Scopes patch

--- src/xrGame/WeaponMagazined.cpp
+++ src/xrGame/WeaponMagazined.cpp
@@ -1316,6 +1316,7 @@ bool CWeaponMagazined::Detach(const char* item_section_name, bool b_spawn_item)
 		return inherited::Detach(item_section_name, b_spawn_item);;
 }
 
+extern int scope_2dtexactive; //crookr
 void CWeaponMagazined::InitAddons()
 {
 	if (IsScopeAttached())
@@ -1332,6 +1333,7 @@ void CWeaponMagazined::InitAddons()
 			if (m_UIScope)
 			{
 				xr_delete(m_UIScope);
+				scope_2dtexactive = 0;//crookr
 			}
 
 			if (!g_dedicated_server && scope_tex_name != NULL)
@@ -1348,6 +1350,7 @@ void CWeaponMagazined::InitAddons()
 		if (m_eScopeStatus != ALife::eAddonPermanent && m_UIScope)
 		{
 			xr_delete(m_UIScope);
+			scope_2dtexactive = 0;//crookr
 		}
 		ResetScopeKoeffs();
 	}
